61b6a01fc33d6bd69e0b846e9c33e5fabf216ad6
smola93 <mateuszsamulnik@gmail.com>
2021-06-24 20:45:31 +0200

    Deleted Unnecessary comments and "getResponseString" method structure.

--- src/main/java/com/exchange/exchangeWS/Currency.java
+++ src/main/java/com/exchange/exchangeWS/Currency.java
@@ -34,9 +34,6 @@ public class Currency {
 
     String getResponseString(String url) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-        //add headers to the connection, or check the status if desired..
-
-        // handle error response code it occurs
         int responseCode = connection.getResponseCode();
         InputStream inputStream;
         if (200 <= responseCode && responseCode <= 299) {
@@ -44,20 +41,12 @@ public class Currency {
         } else {
             inputStream = connection.getErrorStream();
         }
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        inputStream));
-
+        BufferedReader in = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder response = new StringBuilder();
         String currentLine;
-
         while ((currentLine = in.readLine()) != null)
             response.append(currentLine);
-
         in.close();
-
         return response.toString();
     }
-
 }
