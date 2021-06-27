a88615176ed4a691b7dbd17794c09bfdf647d044
smola93 <mateuszsamulnik@gmail.com>
2021-06-27 18:10:06 +0200

    Add view models ignoring

--- src/main/java/com/exchange/exchangeWS/util/SwaggerConfig.java
+++ src/main/java/com/exchange/exchangeWS/util/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.exchange.exchangeWS.util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ModelAndView;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -18,6 +19,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .ignoredParameterTypes(ModelAndView.class)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
